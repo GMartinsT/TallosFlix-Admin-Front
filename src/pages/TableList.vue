610c064524dd7d8f8f60123fd6c764db8eaac3a4
Guilherme Martins <guilherme.martinst.dev@gmail.com>
2023-07-25 16:56:34 -0300

    Refactor: refatoração da lista de tabelas para a tabela de usuários

--- src/pages/TableList.vue
+++ src/pages/TableList.vue
@@ -3,113 +3,57 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-12">
-          <card class="strpied-tabled-with-hover"
-                body-classes="table-full-width table-responsive"
-          >
+          <card class="userCard">
             <template slot="header">
-              <h4 class="card-title">Striped Table with Hover</h4>
-              <p class="card-category">Here is a subtitle for this table</p>
+              <h4 class="card-title">Usuários</h4>
+              <p class="card-category">Lista dos usuários cadastrados</p>
             </template>
-            <l-table class="table-hover table-striped"
-                     :columns="table1.columns"
-                     :data="table1.data">
-            </l-table>
           </card>
-
+          <div>
+            <generic-table>
+              <template v-slot:header>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Email</th>
+              </template>
+              <template>
+                <tr v-for="user in users" :key="user.id">
+                  <td>{{ user.id }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.email }}</td>
+                </tr>
+              </template>
+            </generic-table>
+          </div>
         </div>
-
-        <div class="col-12">
-          <card class="card-plain">
-            <template slot="header">
-              <h4 class="card-title">Table on Plain Background</h4>
-              <p class="card-category">Here is a subtitle for this table</p>
-            </template>
-            <div class="table-responsive">
-              <l-table class="table-hover"
-                       :columns="table2.columns"
-                       :data="table2.data">
-              </l-table>
-            </div>
-          </card>
-        </div>
-
-        <div class="col-12">
-          <card class="strpied-tabled-with-hover"
-                body-classes="table-full-width table-responsive"
-          >
-            <template slot="header">
-              <h4 class="card-title">Small table</h4>
-              <p class="card-category">Here is a subtitle for this table</p>
-            </template>
-            <l-table class="table-hover table-striped table-sm"
-                     :columns="table1.columns"
-                     :data="table1.data">
-            </l-table>
-          </card>
-
-        </div>
-
       </div>
     </div>
   </div>
 </template>
 <script>
-  import LTable from 'src/components/Table.vue'
-  import Card from 'src/components/Cards/Card.vue'
-  const tableColumns = ['Id', 'Name', 'Salary', 'Country', 'City']
-  const tableData = [{
-    id: 1,
-    name: 'Dakota Rice',
-    salary: '$36.738',
-    country: 'Niger',
-    city: 'Oud-Turnhout'
+import Card from "src/components/Cards/Card.vue";
+import GenericTable from "src/components/GenericTable.vue";
+export default {
+  components: {
+    Card,
+    GenericTable,
   },
-  {
-    id: 2,
-    name: 'Minerva Hooper',
-    salary: '$23,789',
-    country: 'Curaçao',
-    city: 'Sinaai-Waas'
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Usuário 1", email: "user1@example.com" },
+        { id: 2, name: "Usuário 2", email: "user2@example.com" },
+      ],
+    };
   },
-  {
-    id: 3,
-    name: 'Sage Rodriguez',
-    salary: '$56,142',
-    country: 'Netherlands',
-    city: 'Baileux'
-  },
-  {
-    id: 4,
-    name: 'Philip Chaney',
-    salary: '$38,735',
-    country: 'Korea, South',
-    city: 'Overland Park'
-  },
-  {
-    id: 5,
-    name: 'Doris Greene',
-    salary: '$63,542',
-    country: 'Malawi',
-    city: 'Feldkirchen in Kärnten'
-  }]
-  export default {
-    components: {
-      LTable,
-      Card
-    },
-    data () {
-      return {
-        table1: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        },
-        table2: {
-          columns: [...tableColumns],
-          data: [...tableData]
-        }
-      }
-    }
-  }
+};
 </script>
 <style>
+.userCard {
+  margin-bottom: 10px;
+  padding-bottom: 5px;
+}
+th {
+  text-align: center !important;
+}
 </style>
