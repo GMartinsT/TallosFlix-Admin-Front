672cf3fbfff108e27467832007530756e68f9ddf
Guilherme Martins <guilherme.martinst.dev@gmail.com>
2023-07-25 16:54:37 -0300

    Feat: componentização da tabela

---
+++ src/components/GenericTable.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="table-responsive">
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <slot name="header" />
+        </tr>
+      </thead>
+      <tbody>
+        <slot />
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GenericTable",
+};
+</script>
+
+<style></style>
