e0ae9098f5f99f35272b4bbc88a752570b192fcb
Guilherme Martins <guilherme.martinst.dev@gmail.com>
2023-08-07 17:07:25 -0300

    Feat: formulario de edição componentizado

---
+++ src/components/UpdateForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <slot />
+  </form>
+</template>
+
+<script>
+export default {
+  name: "EditForm",
+  methods: {
+    submitForm() {
+      this.$emit("submit");
+    },
+  },
+};
+</script>
+
+<style></style>
