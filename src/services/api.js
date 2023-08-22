e26335a2321daa2a17668457daa9d5abb9473478
Guilherme Martins <guilherme.martinst.dev@gmail.com>
2023-08-22 14:49:14 -0300

    env: adição de arquivos no gitignore e alteração da baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import store from 'src/store/store.js';
 
 export const ApiService = axios.create({
-  //baseURL: "http://localhost:8000/moviesapi/",
-  baseURL: "http://localhost:3010",
+  baseURL: "http://localhost:8000/moviesapi/",
+  //baseURL: "https://localhost:3010",
+  headers: {"Access-Control-Allow-Origin": "*"}
 });
 
 ApiService.interceptors.request.use((config) => {
