389c77aca3d86d7c63898c9abff75e1bff5837ad
Guilherme Martins <guilherme.martinst.dev@gmail.com>
2023-08-23 16:44:34 -0300

    chore: alterando baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from 'src/store/store.js';
 
 export const ApiService = axios.create({
-  baseURL: "http://localhost:8000/moviesapi/",
+  baseURL: "http://localhost:8000/moviesapi",
   //baseURL: "https://localhost:3010",
   headers: {"Access-Control-Allow-Origin": "*"}
 });
